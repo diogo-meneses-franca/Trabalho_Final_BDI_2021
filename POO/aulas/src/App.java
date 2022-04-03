67e6333ee1398810f1e7a7e80732784127e2a36f
Diogo Meneses França <diogo.meneses.franca@gmail.com>
2022-04-03 20:31:51 -0300

    iniciando diretorio

--- POO/aulas/src/App.java
+++ POO/aulas/src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Scanner t = new Scanner(System.in);
+            int A = t.nextInt();
+            int B = t.nextInt();        
+            System.out.println(A + B);
+        }
     }
+
+class Main {
+    public static void main(String[] args){
+        int A = 2;
+        int B = 3;
+        int C = A + B;
+        System.out.println(C);
+    }
+    
 }
